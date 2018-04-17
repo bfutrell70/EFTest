6a51d24f4c245602a0fab4dbc2fd44eab5d383ce
Brian Futrell <bfutrel@gmail.com>
2018-04-17 09:34:40 -0400

    initial commit

---
+++ EFTest/Models/Connector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFTest.Models
+{
+    public class Connector
+    {
+        public int uid { get; set; }
+
+        public string Name { get; set; }
+
+        public string sku { get; set; }
+
+
+    }
+}
---
+++ EFTest/Models/Lookup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFTest.Models
+{
+    public class Lookup
+    {
+        public string sku { get; set; }
+
+        public decimal Price { get; set; }
+
+        public decimal Weight { get; set; }
+
+
+    }
+}
