8f88e7dc8b839ca50c36cd43f5bacffaf7db6e8e
Brian Futrell <bfutrel@gmail.com>
2018-04-17 09:49:41 -0400

    working on data context

---
+++ EFTest/Models/DbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFTest.Models
+{
+    public class EFTestContext : DbContext
+    {
+        public IDbSet<Plug> Plugs { get; set; }
+
+        public IDbSet<Connector> Connectors { get; set; }
+
+        public IDbSet<Cord> Cords { get; set; }
+
+        public IDbSet<Lookup> Lookups { get; set; }
+
+        public EFTestContext(string connectionString) : base(connectionString)
+        {
+            //Database.SetInitializer<DefaultContext>(null);
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+
+
+    }
+}
