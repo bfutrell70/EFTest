7b28eff9dac4f4a4b3447f1a600e72dc91c0c661
Brian Futrell <bfutrel@gmail.com>
2018-05-04 08:58:30 -0400

    added Key attributes to component and lookup classes

--- EFTest/Models/Cord.cs
+++ EFTest/Models/Cord.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace EFTest.Models
 {
     public class Cord
     {
+        [Key]
         public int uid { get; set; }
 
         public string Name { get; set; }
--- EFTest/Models/Lookup.cs
+++ EFTest/Models/Lookup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace EFTest.Models
 {
     public class Lookup
     {
+        [Key]
         public string sku { get; set; }
 
         public decimal Price { get; set; }
