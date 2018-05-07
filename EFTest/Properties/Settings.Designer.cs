2adc1c0a3f19537f36d2fab3088e4860dd4417fb
Brian Futrell <bfutrel@gmail.com>
2018-05-07 11:14:45 -0400

    changes from creating model

--- EFTest/Properties/Settings.Designer.cs
+++ EFTest/Properties/Settings.Designer.cs
@@ -26,8 +26,8 @@ namespace EFTest.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=Data\\EFTest.mdf;Integrated Se" +
-            "curity=True;User Instance=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Brian\\source\\repos\\E" +
+            "fTest\\EFTest\\Data\\EFTest.mdf;Integrated Security=True")]
         public string EFTestConnectionString {
             get {
                 return ((string)(this["EFTestConnectionString"]));
